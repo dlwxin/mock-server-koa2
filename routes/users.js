a7adb69fedca23e5fd2c43445d58632272cdab47
dlwxin <dlwxin@outlook.com>
2021-12-11 23:36:05 +0800

    chore: add example for router

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,12 @@ const router = require('koa-router')()
 
 router.prefix('/users')
 
+// curl http://localhost:3000/users
 router.get('/', function (ctx, next) {
   ctx.body = 'this is a users response!'
 })
 
+// curl http://localhost:3000/users/bar
 router.get('/bar', function (ctx, next) {
   ctx.body = 'this is a users/bar response'
 })
